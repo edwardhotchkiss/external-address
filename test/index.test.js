9a52ce71dbbc33ee2885050c3d9fb67e778c7b95
Edward Hotchkiss <e@ingk.com>
2011-12-24 15:43:54 -0500

    initial setup for lookup() requesting data

--- test/index.test.js
+++ test/index.test.js
@@ -5,15 +5,28 @@
 
   var vows = require('vows'),
       assert = require('assert'),
-      address = require('../lib/external-address'); 
+      external_address = require('../lib/external-address');
+
+/*!
+  Add Tests to Suite
+ */
 
 vows.describe('basic module tests').addBatch({
   'when instantiating `external-address`':{
     topic:function(){
-      return address
+      return external_address;
     },
     '`address` should be an object':function(topic){
       assert.isObject(topic);
+    }
+  },
+  'when fetching my address with external_address.lookup()':{
+    topic:function(){
+      external_address.lookup(this.callback);
+    },
+    'there should be no errors':function(error, address){
+      assert.isNull(error);
+      assert.isDefined(address);
     }
   }
 }).export(module);
